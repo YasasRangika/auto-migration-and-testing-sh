d88d0c959a954b62cef15cbbe66926779a3ccc4b
Yasas <rangikaymendis@gmail.com>
2018-11-28 17:00:12 +0530

    Re-indexing the artifacts in the registry.xml

--- check.sh
+++ check.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-<<comment
-[[ $(ls -A /home/yasas/Documents/WSO2/wso2am-2.5.0/repository/tenants) ]] && for output in /home/yasas/Documents/WSO2/wso2am-2.5.0/repository/tenants/*
-do
-    echo $output
-done
-comment
-
-echo yasas
-
+if [ -e ../solr ] 
+then
+  echo Path exists
+fi
--- run.sh
+++ run.sh
@@ -364,6 +364,24 @@ esac
 
 
 
+#Re-indexing the artifacts in the registry.xml
+echo "*****************************Re-indexing the artifacts in the registry*********************************"
+sed -i.bak 's/\/_system\/local\/repository\/components\/org.wso2.carbon.registry\/indexing\/lastaccesstime/\/_system\/local\/repository\/components\/org.wso2.carbon.registry\/indexing\/lastaccesstime_1/' ../repository/conf/registry.xml
+if [ $? -eq 0 ]
+then
+	echo "Re-indexing the artifacts in the registry.xml is successful."
+else
+	echo "Configuration failed, Please manually rename the <lastAccessTimeLocation> element in the <API-M_2.x.0_HOME>/repository/conf/registry.xml file."
+fi
+
+if [ ! -e "" ] 
+then
+  echo Path does not exists
+fi
+
+
+
+
 
 
 
